243adb42e6cdbc3e74ba56e90da24eb1ce8cba95
Koding Kidd <105122189+kodingfinesse@users.noreply.github.com>
2024-02-01 14:07:32 -0800

    Update gentoo-install.sh

--- gentoo-install.sh
+++ gentoo-install.sh
@@ -4,33 +4,37 @@
 
 # Chroot
 ## Please, run this right after running script 00
-mount -o defaults,relatime,compress=lzo,autodefrag,subvol=root /dev/nvme0n1p4 /mnt/gentoo
+mount -o defaults,relatime,compress=lzo,autodefrag,subvol=root /dev/vda4 /mnt/gentoo
 
 ## create dirs for mounts
 cd /mnt/gentoo
 mkdir srv home root var boot
 
 ## mount
-mount -o defaults,relatime,compress=lzo,autodefrag,subvol=home /dev/nvme0n1p4 /mnt/gentoo/home
-mount -o defaults,relatime,compress=lzo,autodefrag,subvol=srv /dev/nvme0n1p4 /mnt/gentoo/srv
-mount -o defaults,relatime,compress=lzo,autodefrag,subvol=var /dev/nvme0n1p4 /mnt/gentoo/var
-mount -o defaults,relatime /dev/nvme0n1p2 /mnt/gentoo/boot
+mount -o defaults,relatime,compress=lzo,autodefrag,subvol=home /dev/vda4 /mnt/gentoo/home
+mount -o defaults,relatime,compress=lzo,autodefrag,subvol=srv /dev/vda4 /mnt/gentoo/srv
+mount -o defaults,relatime,compress=lzo,autodefrag,subvol=var /dev/vda4 /mnt/gentoo/var
+mount -o defaults,relatime /dev/vda2 /mnt/gentoo/boot
 
 ### efi partition
-mount -o defaults,noatime /dev/nvme0n1p1 /mnt/gentoo/boot/efi
+mount -o defaults,noatime /dev/vda1 /mnt/gentoo/boot/efi
 
 ## get gentoo stage3
-wget https://distfiles.gentoo.org/releases/amd64/autobuilds/20231217T170203Z/stage3-amd64-desktop-openrc-20231217T170203Z.tar.xz
+curl -LO https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20211121T170545Z/stage3-amd64-nomultilib-systemd-20211121T170545Z.tar.xz
 
 ## uncompress
-tar -xapf stage3-amd64-desktop-openrc-20231217T170203Z.tar.xz
-rm -f stage3-amd64-desktop-openrc-20231217T170203Z.tar.xz
+tar -xapf stage3-amd64-nomultilib-systemd-20211121T170545Z.tar.xz
+rm -f $_
 
 ## mount proc, sys and dev
-mount --types proc /proc /mnt/gentoo/proc && mount --rbind /sys /mnt/gentoo/sys && mount --make-rslave /mnt/gentoo/sys && mount --rbind /dev /mnt/gentoo/dev && mount --make-rslave /mnt/gentoo/dev
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
 
 ## activate swap
-swapon /dev/nvme0n1p3
+swapon /dev/vda3
 
 ## get dns
 cp -u /etc/resolv.conf /mnt/gentoo/etc/
@@ -69,43 +73,47 @@ emaint sync -r gentoo
 cat << 'EOF' > /etc/fstab
 # <fs>      <mountpoint>    <type>  <opts>                                              <dump/pass>
 shm         /dev/shm        tmpfs   nodev,nosuid,noexec                                 0 0
-/dev/nvme0n1p4   /               btrfs   rw,relatime,compress=zstd:1,autodefrag,subvol=root   0 0
-/dev/nvme0n1p4   /home           btrfs   rw,relatime,compress=zstd:1,autodefrag,subvol=home   0 0
-/dev/nvme0n1p4   /srv            btrfs   rw,relatime,compress=zstd:1,autodefrag,subvol=srv    0 0
-/dev/nvme0n1p4   /var            btrfs   rw,relatime,compress=zstd:1,autodefrag,subvol=var    0 0
-/dev/nvme0n1p3   none            swap    sw                                                  0 0
-/dev/nvme0n1p2   /boot           btrfs   rw,relatime                                          1 2
-/dev/nvme0n1p1   /boot/efi       vfat    umask=0077                         0 2
+/dev/vda4   /               btrfs   rw,relatime,compress=zstd:1,autodefrag,subvol=root   0 0
+/dev/vda4   /home           btrfs   rw,relatime,compress=zstd:1,autodefrag,subvol=home   0 0
+/dev/vda4   /srv            btrfs   rw,relatime,compress=zstd:1,autodefrag,subvol=srv    0 0
+/dev/vda4   /var            btrfs   rw,relatime,compress=zstd:1,autodefrag,subvol=var    0 0
+/dev/vda3   none            swap    sw                                                  0 0
+/dev/vda2   /boot           btrfs   rw,relatime                                          1 2
+/dev/vda1   /boot/efi       vfat    umask=0077,shortname=winnt                          0 2
 EOF
 
 ## local time in MX
-ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime
 
 ## add cpu flags
 emerge app-portage/cpuid2cpuflags
+cpu_flags=$( cpuid2cpuflags | cut -d ' ' -f '2-' )
+
 ## append to my make.conf
-GNU nano 7.2                            make.conf
-GNU nano 7.2                                                 make.conf
-use_remove="-accessibility -altivec -apache2 -aqua -big-endian -bindist -boundschecking -bsf -canna -clamav -connman -coreaudio -c>
-use_add="symlink unicode"
-CFLAGS="-march=znver3 -mtune=znver3 -O3 -pipe"
-CXXFLAGS=${CFLAGS}
+use_remove='-accessibility -altivec -apache2 -aqua -big-endian -bindist -boundschecking -bsf -canna -clamav -connman -coreaudio -custom-cflags -debug -dedicated -emacs -handbook -ibm -infiniband -iwmmxt -kde -kontact -libav -libedit -libressl -libsamplerate -mono -mule -neon -oci8 -oci8-instant-client -oracle -oss -pch -pcmcia -plasma -qmail-spp -qt4 -qt5 -static -syslog -sysvipc -tcpd -xemacs -yahoo -zsh-completion'
+use_add='symlink unicode vim-syntax'
+make_opts="-j$(( $( nproc ) + 1 ))"
+
+cat << EOF > /etc/portage/make.conf
+CFLAGS="-mtune=native -O2 -pipe"
+CXXFLAGS=\${CFLAGS}
 CHOST="x86_64-pc-linux-gnu"
-CPU_FLAGS_X86="aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt rdrand sha sse sse2 sse3 sse4_1 sse4_2 sse4a ssse3"
+CPU_FLAGS_X86="${cpu_flags}"
 GRUB_PLATFORMS="efi-64"
-ACCEPT_KEYWORDS="~amd64"
-MAKEOPTS="--jobs 13 --load-average 9"
+# enable this if you like living on the edge
+#ACCEPT_KEYWORDS="~amd64"
+MAKEOPTS="${make_opts}"
 ADD="${use_add}"
 REMOVE="${use_remove}"
 USE="\$REMOVE \$ADD"
 # Portage Opts
 FEATURES="parallel-fetch parallel-install ebuild-locks"
-EMERGE_DEFAULT_OPTS="--getbinpkg --binpkg-respect-use=y --with-bdeps=y"
+EMERGE_DEFAULT_OPTS="--with-bdeps=y"
 AUTOCLEAN="yes"
-
+EOF
 
 ## set profiles
-eselect profile set default/linux/amd64/23.0/desktop/kde
+eselect profile set default/linux/amd64/17.1/no-multilib/systemd/merged-usr
 
 ## update everything and cleanup
 emerge -DNju @world
@@ -134,11 +142,29 @@ emerge -DNju genkernel sys-kernel/gentoo-sources sys-kernel/linux-firmware
 ##	 * btrfs support
 genkernel --menuconfig --btrfs --virtio all
 
+# Configure real root and stuff
+# Do not forget to append systemd to the kernel command line: GRUB_CMDLINE_LINUX="real_init=/lib/systemd/systemd quiet"
+vim /etc/default/grub
+
 ## install grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi
 
 # update grub
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# systemd
+## Setup machine ID to activate journaling
+systemd-machine-id-setup
+
+## DHCP
+cat << 'EOF' > /etc/systemd/network/20-default.network
+[Match]
+Name = enp*
+[Network]
+DHCP = yes
+EOF
+
+systemctl enable systemd-networkd.socket
 
 # set root password
 passwd
